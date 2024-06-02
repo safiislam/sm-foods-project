163250580764945c20d9f5d0a40b19a2f22ac70b
Mdsakib999 <sakibabrar999@gmail.com>
2024-06-02 13:01:37 -0700

    Product tag like payment and delevary

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,6 +58,43 @@ const Home = () => {
 				</div>
 			</div>
 		</section>
+
+
+
+
+		{/* section for every product */}
+        <div className=' bg-gray-100 lg:flex justify-evenly py-8 text-center mb-1'>
+            <div className=' lg:flex items-center gap-x-4 py-10 px-3 '>
+                <div className='w-[100px] mx-auto lg:mx-0 mb-3 lg:mb-0'>
+                    <img className='rounded-full w-full' src="https://img.freepik.com/free-vector/cash-payment-concept-illustration_114360-3320.jpg?t=st=1717058911~exp=1717062511~hmac=2dd39ba50f2d6047a1de116b053e34a4a834e3d519a859aab8c25f98d6057101&w=826" alt="Paymant picture" />
+                </div>
+                <div>
+                    <p className='font-semibold mb-1'>নিরাপদ পেমেন্ট</p>
+                    <p>বিভিন্ন পেমেন্ট পদ্ধতি থেকে বেছে নিন</p>
+                </div>
+            </div>
+
+            <div className=' lg:flex items-center gap-x-4 py-10 px-3'>
+                <div className='w-[100px]  mx-auto lg:mx-0 mb-3 lg:mb-0'>
+                    <img className='rounded-full w-full' src="https://img.freepik.com/free-vector/delivery-service-illustrated_23-2148505081.jpg?t=st=1717062905~exp=1717066505~hmac=4486ebf5858860e3edefa1a839d12bb164b03637f97b9b509059f6450833e475&w=826" alt="Paymant picture" />
+                </div>
+                <div>
+                    <p className='font-semibold mb-1'>ডেলিভারি</p>
+                    <p>৩-৫ দিনের মধ্যে আপনার পণ্য পৌছে যাবে</p>
+                </div>
+            </div>
+
+            <div className=' lg:flex items-center gap-x-4 py-10 px-3'>
+                <div className='w-[100px] mx-auto lg:mx-0 mb-3 lg:mb-0'>
+                    <img className='rounded-full w-full' src="https://img.freepik.com/premium-vector/100-organic-food-product-sticker-certified-label_236275-45.jpg?ga=GA1.1.377660834.1717058610&semt=sph" alt="Paymant picture" />
+                </div>
+                <div>
+                    <p className='font-semibold mb-1'>১০০% ন্যাচারাল</p>
+                    <p>প্রাকৃতিক উপাদান ব্যবহার করতে আমরা প্রতিশ্রুতিবদ্ধ</p>
+                </div>
+            </div>
+
+        </div>
         </div>
     );
 };
