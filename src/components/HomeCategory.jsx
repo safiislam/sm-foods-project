8d304c816d3a9da3184b17998a0b7ddf342b4226
Mdsakib999 <sakibabrar999@gmail.com>
2024-06-23 18:41:30 -0700

    changes on home category

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -28,7 +28,7 @@ const HomeCategory = () => {
                 className=" bg-white dark:bg-slate-800 shadow-xl relative flex items-end justify-center min-h-[160px] rounded-t-[90px] rounded-b-[15px] border dark:border-slate-700 "
               > 
                 <div className=" absolute -top-[75px] left-1/2 -translate-x-1/2 bg-white dark:bg-slate-800 shadow border dark:border-slate-700 rounded-full flex justify-center items-center h-40 w-40">
-                  <img src="https://greenmartbd.net/storage/upload/product/phT3W2MDVDODfNv1lZ62K8qedUEAIHbtD5ouqvM5.png" alt="" className="w-20" />
+                  <img src="https://greenmartbd.net/storage/upload/product/phT3W2MDVDODfNv1lZ62K8qedUEAIHbtD5ouqvM5.png" alt="tel ar picture" className="w-20" />
                 </div>
                 <h4 className="text-2xl font-medium mb-5">MUSTARD OIL / সরিষার তেল</h4>
               </Link>
