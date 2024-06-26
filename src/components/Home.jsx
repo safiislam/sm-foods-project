8355b65d856fc45c22d9593752de53a2afb633a4
Mdsakib999 <sakibabrar999@gmail.com>
2024-06-25 18:04:10 -0700

    home slider responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "./Home.css";
+
 
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import HomeCustomerReview from "./HomeCustomerReview";
 
 const Home = () => {
   return (
     <div>
+      {/* Home Slider */}
       <section className=" w-[90%] mx-auto z-0  md:mt-1">
         <Swiper
           spaceBetween={30}
@@ -50,15 +54,15 @@ const Home = () => {
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className=" md:flex justify-evenly items-center md:py-32 bg-slate-50">
+            <div className=" md:flex justify-evenly items-center md:py-24 py-6 bg-slate-50">
               <img
                 src="https://basshopbd.com/wp-content/uploads/2022/09/d25822560b58a13c05c54e98d042399b_1000x1000.png"
                 alt="Mustard Oil"
-                className="md:w-[500px]  rounded-xl shadow-lg "
+                className="md:w-[440px] w-[90%] mx-auto md:mx-0  rounded-xl shadow-lg "
               />
 
               {/* Middle Section: Product Image and Key Benefits */}
-              <div className=" w-[90%] mx-auto md:mx-0 mt-12 md:mt-0 w md:w-[45%] max-w-4xl flex flex-col md:flex-row md:items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8 relative">
+              <div className=" w-[90%] mx-auto md:mx-0 mt-10 md:mt-0 w md:w-[45%] max-w-4xl flex flex-col md:flex-row md:items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8 relative">
                 <div className="py-8 md:pl-10 pl-4 ">
                   <p className="md:text-5xl text-3xl font-bold text-yellow-500 md:mb-10 mb-6">
                     দেশি সরিষার তেল
@@ -78,26 +82,30 @@ const Home = () => {
                   </button>
                 </div>
 
-                <img className="md:w-[180px] hidden md:block absolute  md:right-24 md:bottom-20 " src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png" alt="" />
+                <img
+                  className="md:w-[160px] hidden md:block absolute  md:right-20 md:bottom-16 "
+                  src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png"
+                  alt=""
+                />
               </div>
             </div>
           </SwiperSlide>
 
           <SwiperSlide>
-		  <div className=" flex justify-evenly items-center md:py-32 bg-slate-50">
+            <div className=" md:flex justify-evenly items-center md:py-24 py-6 bg-slate-50">
               <img
                 src="https://www.harniva.com/assets/backend/admin/plugins/source/hakkimizda/kalitelibal.png"
                 alt="Mustard Oil"
-                className="w-[500px] rounded-xl shadow-lg"
+                className="md:w-[440px] w-[90%] mx-auto md:mx-0 rounded-xl shadow-lg"
               />
 
               {/* Middle Section: Product Image and Key Benefits */}
-              <div className=" w-[45%] max-w-4xl flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8 relative">
-                <div className="md:py-8 pl-10 ">
-                  <p className="text-5xl font-bold text-yellow-500 mb-10">
-				  Honey / মধু
+              <div className=" w-[90%] mx-auto md:mx-0 mt-10 md:mt-0 w md:w-[45%] max-w-4xl flex flex-col md:flex-row md:items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8 relative">
+                <div className="py-8 md:pl-10 pl-4 ">
+                  <p className="md:text-5xl text-3xl font-bold text-yellow-500 md:mb-10 mb-6">
+                    Honey / মধু
                   </p>
-                  <p className="text-3xl font-bold text-yellow-800 mb-6">
+                  <p className="text-xl md:text-3xl font-bold text-yellow-800 mb-6">
                     100% Pure Outhantic Honey
                   </p>
                   <ul className="text-lg text-gray-700">
@@ -111,26 +119,30 @@ const Home = () => {
                     Shop Now
                   </button>
                 </div>
-                <img className="md:w-[180px] absolute md:right-24 md:bottom-20 " src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png" alt="" />
+                <img
+                  className="md:w-[160px] hidden md:block absolute  md:right-20 md:bottom-16 "
+                  src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png"
+                  alt=""
+                />
               </div>
-        	</div>
+            </div>
           </SwiperSlide>
 
           <SwiperSlide>
-		  <div className=" flex justify-evenly items-center md:py-32 bg-slate-50">
+            <div className=" md:flex justify-evenly items-center md:py-24 py-6 bg-slate-50">
               <img
                 src="https://media.e-valy.com/cms/products/images/f4b3265d-df5e-4835-add4-0cd2654235d3"
-                alt="Ghee"
-                className="w-[500px] rounded-xl shadow-lg"
+                alt="Mustard Oil"
+                className="md:w-[440px] w-[90%] mx-auto md:mx-0 rounded-xl shadow-lg "
               />
 
               {/* Middle Section: Product Image and Key Benefits */}
-              <div className=" relative w-[45%] max-w-4xl flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8">
-                <div className="md:py-8 pl-10 ">
-                  <p className="text-5xl font-bold text-yellow-500 mb-10">
-				  গাওয়া ঘি / Gawa Ghee
+              <div className=" w-[90%] mx-auto md:mx-0 mt-10 md:mt-0 w md:w-[45%] max-w-4xl flex flex-col md:flex-row md:items-center bg-white shadow-lg rounded-lg overflow-hidden mb-8 relative">
+                <div className="py-8 md:pl-10 pl-4 ">
+                  <p className="md:text-5xl text-3xl font-bold text-yellow-500 md:mb-10 mb-6">
+                    গাওয়া ঘি / Gawa Ghee
                   </p>
-                  <p className="text-3xl font-bold text-yellow-800 mb-6">
+                  <p className="text-xl md:text-3xl font-bold text-yellow-800 mb-6">
                     100% Pure Gawa Ghee
                   </p>
                   <ul className="text-lg text-gray-700">
@@ -140,18 +152,18 @@ const Home = () => {
                     <li className="mb-2 font-semibold">✅ Organic</li>
                   </ul>
 
-                  <button className="px-8 rounded-ss-xl rounded-ee-xl py-3  mt-6 text-white  font-bold text-base bg-gradient-to-r from-orange-500 to-yellow-300 hover:from-yellow-300 hover:to-orange-500">
+                  <button className="px-8 rounded-ss-xl rounded-ee-xl py-3  mt-6 text-white  font-bold text-base bg-gradient-to-r from-orange-700 to-yellow-400 hover:from-yellow-500 hover:to-orange-700">
                     Shop Now
                   </button>
                 </div>
-                <img className="md:w-[180px] absolute right-24 bottom-20 " src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png" alt="" />
+
+                <img
+                  className="md:w-[160px] hidden md:block absolute  md:right-20 md:bottom-16 "
+                  src="https://i.ibb.co/3c8MnPc/SM-Foods-logo-removebg-preview.png"
+                  alt=""
+                />
               </div>
-        	</div>
-            {/* <img
-              className="w-[400px]"
-              src="https://i.ibb.co/CsNtTfM/Untitleddesign-86-removebg-preview.png"
-              alt=""
-            /> */}
+            </div>
           </SwiperSlide>
         </Swiper>
       </section>
@@ -159,6 +171,16 @@ const Home = () => {
       {/* Product category in Home */}
       <section>
         <HomeCategory></HomeCategory>
+      </section>
+
+      {/* Our Popular Product */}
+      <section>
+
+      </section>
+
+      {/* Customer review */}
+      <section>
+        <HomeCustomerReview></HomeCustomerReview>
       </section>
     </div>
   );
